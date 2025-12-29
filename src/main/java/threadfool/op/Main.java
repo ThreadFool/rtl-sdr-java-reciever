a84f50f183672bb1b69de7919f644c1ad31f66c2
I583075 <rafal.szygula@sap.com>
2025-12-29 17:09:35 +0100

    working jni

--- src/main/java/threadfool/op/Main.java
+++ src/main/java/threadfool/op/Main.java
@@ -3,4 +3,12 @@ package threadfool.op;
 public class Main
 {
 
+	static {
+		System.loadLibrary("rtlsdr_jni");
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("JNI works");
+	}
 }
